56e39f37e536eba52b11011652bfa24a6234878f
wobble-house <ross@wobble.house>
2023-09-08 12:10:42 -0400

    hoping to escape apos easier this way

--- components/nav.tsx
+++ components/nav.tsx
@@ -160,7 +160,7 @@ export default function Nav({children}) {
               </ul>
               </div>
               <div className="p-16 bg-transparent bg-opacity-50 border-solid border-white border-[10px]">
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              <p>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`}</p>
           </div>
           <div className="flex flex-row justify-between pt-10">
             <p>{`(248) 410-2715`}</p>
