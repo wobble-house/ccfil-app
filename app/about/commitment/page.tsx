ffff18653c19417bb59ee00332c35729dc7bdb1e
wobble-house <ross@wobble.house>
2023-09-12 11:21:41 -0400

    started sticky info component

--- app/about/commitment/page.tsx
+++ app/about/commitment/page.tsx
@@ -2,7 +2,7 @@ import "server-only"
 import { Animation } from '@/utils/animation/animation'
 import { Suspense } from "react"
 import Loading from "@/app/loading"
-import InfoSegment from "@/components/info-segment"
+import InfoSegment, { StickyInfoSegment, InfoBody } from "@/components/info-segment"
 
 export const metadata = {
   title: 'Commitment',
@@ -12,16 +12,27 @@ export const metadata = {
 const headerText =`We are committed to enhancing the daily lives of all our residents.`
 const bodyText = `Community Connections provides a comprehensive, semi-independent community-based living program for individuals affected by traumatic brain injury. Cognitive, social, vocational, and educational components are integrated into each resident’s individualized treatment plan, which is facilitated by both the resident and our dedicated occupational therapists and rehabilitation specialists.`
 const ImageUrl = `https://ccfil.com/wp-content/uploads/2016/07/CCFILCampusLifeBW.jpg`
+const captionText = "Enriching Our Residents' Lives"
+const stickyHeaderText = `Independence and ownership are encouraged and supported on entry into our program.`
+const stickyBodyText = `Community Connections acknowledges that each resident has unique routines and interests prior to admission; it is our mission to respect, uphold, and enhance the daily lives of our residents while guiding them towards greater self-reliance and community reentry. Each resident frequently meets with our rehabilitation specialists to ensure progress towards their individualized treatment goals while maintaining choice-driven objectives. Community Connections believes that residents’ motivation, achievement, and happiness are maximized by our innovative approach to cognitive-behavioral programming coupled with our compassionate and encouraging support staff.
+
+In addition to numerous opportunities to participate in peer support groups and therapeutic recreational activities, residents of our program are provided with a furnished apartment, utilities, cable/internet services, and transportation to community outings and medical appointments. Community Connections is also distinct from other programs as we welcome spouses and children of our residents. Residents’ family dynamics are incorporated into their Individualized Treatment Plans to ensure continued positive, healthy relationships.
+
+Community Connections provides a supportive living environment through a multidisciplinary approach to community integration. We are committed to excellence to enrich the lives of our residents and the community in which they live.`
+const stickyImageUrl = `https://ccfil.com/wp-content/uploads/2016/07/CCFILCampusLifeTile.jpg`
 
 export default function Commitment() {
+  
   return (
    <>
          <Animation mode={'wait'} initial={false}>
         <Suspense fallback={<Loading/>}>
-          <div className="mx-auto max-w-screen mt-20 py-5">
+          <div className="mx-auto w-full mt-20 py-5">
 
           <InfoSegment headerText={headerText} bodyText={bodyText} ImageUrl={ImageUrl} ButtonUrl={null} Type="A"/>
-
+          <StickyInfoSegment captionText={captionText}>
+          <InfoBody headerText={stickyHeaderText} bodyText={stickyBodyText} ImageUrl={stickyImageUrl} ButtonUrl={null}/>
+          </StickyInfoSegment>
           </div>
         </Suspense>
       </Animation>
--- components/info-segment.tsx
+++ components/info-segment.tsx
@@ -6,7 +6,7 @@ export default function InfoSegment({headerText, bodyText, Type, ImageUrl, Butto
     const router = useRouter();
     return(
         <div className={`z-20 h-full bg-white pb-10 mx-auto `}>
-            <div className={`max-w-6xl flex flex-col w-full h-full ${Type != "B" ? "md:flex-row " : "md:flex-row-reverse"}`}>
+            <div className={`max-w-6xl flex flex-col mx-auto w-full h-full ${Type != "B" ? "md:flex-row " : "md:flex-row-reverse"}`}>
             <div className={`w-1/2 ${Type != "B" ? "ml-5" : "mr-5"}`}>
             <ImageHandler src={ImageUrl} width={800} height={600}/>
             </div>
@@ -21,5 +21,43 @@ export default function InfoSegment({headerText, bodyText, Type, ImageUrl, Butto
             </div>
             </div>
         </div>
+    )
+}
+
+export function StickyInfoSegment({children, captionText}){
+    return(
+        <>
+        <div className="w-full bg-[#f5f5f5]">
+        <div className="flex flex-col md:flex-row max-w-6xl py-16 px-3 gap-10 mx-auto">
+            <div className="w-1/3 border-[10px] border-blue-500 p-10 sticky mb-auto">
+                <h2 className="text-blue-700 font-black text-5xl">
+                    {captionText}
+                </h2>
+            </div>
+            <div className="w-2/3">
+                {children}
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
+
+export function InfoBody({headerText, bodyText,ImageUrl,ButtonUrl }:{headerText: string, bodyText: string, ImageUrl, ButtonUrl}){
+    const router = useRouter();
+    return(
+        <>
+            <div className="flex flex-col gap-7 bg-white">
+            <ImageHandler src={ImageUrl} width={800} height={600}/>
+            <div className="flex flex-col gap-7 p-5">
+            <h2 className="text-2xl font-bold text-blue-700">{headerText}</h2>
+            <hr className="border-red-500 border-2 w-1/2"/>
+            <p>{bodyText}</p>
+            {ButtonUrl == null ? <></> : <button className="uppercase border-2 border-blue-500 rounded-full px-6 py-2 mx-auto hover:bg-blue-500 hover:text-white"
+            onClick={()=> router.push(ButtonUrl)}>Learn More About Us
+            </button> }
+            </div>
+            </div>
+        </>
     )
 }
