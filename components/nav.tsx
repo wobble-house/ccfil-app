98741e2a6a4b998f4490249001ea0ced22778592
wobble.house <ross@wobble.house>
2023-09-25 18:23:46 -0400

    refactoring

--- components/nav.tsx
+++ components/nav.tsx
@@ -10,18 +10,15 @@ export default function Nav({metadata}) {
   const [services, setServices] = useState(false)
   const Navref = useRef();
   const [navbar, setNavbar] = useState(false);
-
-  const handleAboutMouseover = () => {
+  function handleAboutMouseover (){
     setAbout(true);
     setServices(false);
   };
-
-  const handleServicesMouseover = () => {
+  function handleServicesMouseover(){
     setAbout(false);
     setServices(true);
   };
-
-  const handleNavClickOutside = () => {
+  function handleNavClickOutside() {
     setNavbar(false);
   }
   useOnClickOutside(Navref, handleNavClickOutside)
@@ -33,40 +30,14 @@ export default function Nav({metadata}) {
         <div className="flex flex-col md:flex-row flex-grow justify-between z-10 max-w-6xl mx-10 mt-10">
             <div className=" flex flex-col md:flex-row-reverse items-center justify-between md:block">
               <div className="relative z-50 md:hidden">
-                <button
-                  className=" bg-white dark:bg-blue2 px-2
-                 border-2 rounded-md border-blue1 focus:border-2 focus:animate-pulse"
-                 onClick={()=>setNavbar(!navbar)}
-                >
+                <button className=" bg-white dark:bg-blue2 px-2 border-2 rounded-md border-blue1 focus:border-2 focus:animate-pulse"onClick={()=>setNavbar(!navbar)}>
                   {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 border-solid text-blue1 
-                      "
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
+                    <svg xmlns="http://www.w3.org/2000/svg"className="w-6 h-6 border-solid text-blue1"viewBox="0 0 20 20"fill="currentColor">
+                      <path fillRule="evenodd"d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"clipRule="evenodd"/>
                     </svg>
                   ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 border-solid border-1 text-blue1
-                      "
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 border-solid border-1 text-blue1"fill="none"viewBox="0 0 24 24"stroke="currentColor"strokeWidth={2}>
+                      <path strokeLinecap="round"strokeLinejoin="round"d="M4 6h16M4 12h16M4 18h16"/>
                     </svg>
                   )}
                 </button>
