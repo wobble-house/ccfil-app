937dc4f6266df93d520f0fb9c358e8e66868b3c6
wobble-house <ross@wobble.house>
2023-10-18 11:44:32 -0400

    fixing quote escape

--- components/quote.tsx
+++ components/quote.tsx
@@ -9,7 +9,7 @@ export default function QuoteSection({quote, quoteName, quoteImgSrc}:{quote:stri
             <Image src={quoteImgSrc} width={2560} height={1440} alt="background image of smiling people" className="min-h-screen aspect-[16/9] object-cover "/>
         </div>
                 <div className="flex flex-col max-w-3xl bg-transparent mx-auto h-[400px] z-1 justify-center gap-14 -mt-14 pb-10">
-                    <h4 className="text-[180px] font-bold text-white text-right rotate-180 -my-14">"</h4>
+                    <h4 className="text-[180px] font-bold text-white text-right rotate-180 -my-14">&quot;</h4>
         <h3 className="text-left italic font-300 text-3xl text-white">{quote}</h3>
                     <h3 className="text-left font-bold uppercase text-white">
                 {quoteName}
