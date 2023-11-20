07d9984c4cae83199c828e53b98f16ff7a0c4cb3
wobble-house <ross@wobble.house>
2023-11-20 16:49:23 -0500

    updating sendgrid

--- pages/api/sendgrid.ts
+++ pages/api/sendgrid.ts
@@ -5,8 +5,8 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 async function sendEmail(req, res) {
   try {
     await sendgrid.send({
-      to: "marca@ccfil.com", // Your email where you'll receive emails
-      from: "marca@ccfil.com", // your website email address here
+      to: "rford@ccfil.com", // Your email where you'll receive emails
+      from: "tech-support@ccfil.com", // your website email address here
       subject: `[Website Contact]: ${req.body.name}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
