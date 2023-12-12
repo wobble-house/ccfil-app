c60fcbaf22db83cb450247d51a82676c6cfccad0
wobble-house <ross@wobble.house>
2023-12-12 16:46:07 -0500

    force dynamic

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,7 +3,12 @@ import { carouselSlides } from "@/lib/data/data"
 import Nav from "@/components/navigation/nav"
 import { HeaderBGCarousel } from "@/components/header/header-bg-carousel"
 import SignupForm from "@/components/forms/signup-form"
-
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
+export const revalidate = 0
+export const fetchCache = 'auto'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
 export const metadata = {
   title: 'Sign Up',
   description: `Improved quality of life is our set standard; we won't settle for anything less.`
