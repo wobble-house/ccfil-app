7e5cb8eb9d78ecbc5528889dc7bb2f8b55115784
wobble-house <ross@wobble.house>
2023-12-27 10:40:14 -0500

    updating headshot handling for easier updates

--- components/image-handler.tsx
+++ components/image-handler.tsx
@@ -23,3 +23,23 @@ export default function ImageHandler(props){
     );
 };
 
+export function HeadshotHandler(props){
+    const { src, fallbackSrc, alt, ...rest } = props;
+    const [imgSrc, setImgSrc] = useState(src);
+    return (
+        <Image
+            {...rest}
+            src={`https://${process.env.S3_BUCKET}.s3.${process.env.REGION_CODE}.amazonaws.com/public/Images/Headshots/${imgSrc}`}
+            alt={alt}
+            sizes="(max-width: 768px) 100vw,
+            (max-width: 1200px) 50vw,
+            33vw"
+            style={{
+                objectFit: "cover"
+            }}
+            onError={() => {
+                setImgSrc(fallbackSrc);
+            }}
+        />
+    );
+};
