ed06ded740b9663e80ebf2a27c5da0996a842c94
wobble-house <ross@wobble.house>
2024-06-03 15:38:49 -0400

    removed headshot upload button

--- components/cards/profile-heashot.tsx
+++ components/cards/profile-heashot.tsx
@@ -4,22 +4,9 @@ import HeadshotUpload from '../../components/forms/HeadshotUpload'
 import { useState } from 'react';
 
 export default function ProfileHeadshot({user, src, alt}:{user, src: string, alt: string}){
-    const [headshot, setHeadshot] = useState(false)
-
-    const handleClick = async () => {
-      setHeadshot(true)
-    }
-
-    if (headshot) return (
-      <HeadshotUpload user={user}/>
-    )
-
-    else return (
+return (
       <div className="flex flex-col">
       <ThumbnailHandler src={src} alt={alt} fallbackSrc={'oof.png'} className="object-top"/>
-      <button onClick={handleClick}>
-        update
-        </button>
-    </div>
+      </div>
     )
 }
