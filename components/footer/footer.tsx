8060a1705c69afc7f9643d34cc98efd7f3d43408
pkmartus <pkmartus@gmail.com>
2026-07-08 14:32:32 -0400

    link to page

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -72,7 +72,7 @@ Sterling Heights, MI 48312
                 <Button className={"transition ease-in-out duration-[500ms] bg-blue1 hover:px-9 hover:py-2 px-10 py-3 rounded-full uppercase mx-auto hover:text-blue1 hover:bg-transparent font-bold border-solid hover:border-blue1 hover:border-4 text-white"} text={'Contact Us'} link={'/contact'}/>
                 </div>
                 </div>
-                <p className="text-center font-mono text-xs text-white hover:animate-pulse pt-16">©Copyright 2023 Community Connections. All Rights Reserved. Privacy Policy</p>
+                <p className="text-center font-mono text-xs text-white hover:animate-pulse pt-16">©Copyright 2023 Community Connections. All Rights Reserved. <Link href="/privacy-policy">Privacy Policy</Link></p>
             </div>
             
         </div>
